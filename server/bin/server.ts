e26a3db64e2f6ca84a6642e9674eb2c341df5593
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-01-13 20:49:29 -0300

    Creating bootstrap stack

--- server/bin/server.ts
+++ server/bin/server.ts
@@ -3,9 +3,14 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { ControlPlaneStack } from "../lib/control-plane-stack";
 import { ApplicationPlaneStack } from "../lib/app-plane-stack";
+import { BootstrapStack } from "../lib/bootstrap-stack";
 
 const app = new cdk.App();
 const controlPlaneStack = new ControlPlaneStack(app, "ControlPlaneStack");
-new ApplicationPlaneStack(app, "AppPlaneStack", {
+const appPlaneStack = new ApplicationPlaneStack(app, "AppPlaneStack", {
   eventManager: controlPlaneStack.eventManager,
+});
+new BootstrapStack(app, "BootstrapStack", {
+  vpc: appPlaneStack.vpc,
+  codebuildSecurityGroup: appPlaneStack.codebuildSecurityGroup,
 });
