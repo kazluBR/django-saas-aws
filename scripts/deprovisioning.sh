b679b810703ae688b3a2ba6489910607b3ceb856
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2026-01-13 21:29:32 -0300

    Provisioning and deprovisioning implementation

--- scripts/deprovisioning.sh
+++ scripts/deprovisioning.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+if [ "$tier" = "basic" ]; then
+    secret_json=$(aws secretsmanager get-secret-value --secret-id "pool-database-secret" --query SecretString --output text --region "${AWS_REGION}")
+
+    db_host=$(echo "$secret_json" | jq -r '.host')
+    db_user=$(echo "$secret_json" | jq -r '.username')
+    db_password=$(echo "$secret_json" | jq -r '.password')
+    db_port=$(echo "$secret_json" | jq -r '.port')
+
+    mysql -h "$db_host" -u "$db_user" -p"$db_password" -P "$db_port" -e "DROP DATABASE IF EXISTS \`$tenantName\`;"
+fi
+
+aws cloudformation delete-stack --stack-name "tenantTemplateStack-$tenantName"
+aws cloudformation wait stack-delete-complete --stack-name "tenantTemplateStack-$tenantName"
+
+export tenantStatus="deleted"
+export registrationStatus="deleted"
--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -1,2 +1,36 @@
 #!/bin/bash
 
+if [ "$tier" = "basic" ]; then
+    secret_json=$(aws secretsmanager get-secret-value --secret-id "pool-database-secret" --query SecretString --output text --region "${AWS_REGION}")
+
+    db_host=$(echo "$secret_json" | jq -r '.host')
+    db_user=$(echo "$secret_json" | jq -r '.username')
+    db_password=$(echo "$secret_json" | jq -r '.password')
+    db_port=$(echo "$secret_json" | jq -r '.port')
+
+    mysql -h "$db_host" -u "$db_user" -p"$db_password" -P "$db_port" -e "CREATE DATABASE IF NOT EXISTS \`$tenantName\`;"
+fi
+
+bucket_name=$(aws cloudformation describe-stacks \
+  --stack-name BootstrapStack \
+  --query "Stacks[0].Outputs[?OutputKey=='TenantTemplatesBucketName'].OutputValue" \
+  --output text)
+
+bucket_url=$(aws s3 presign "s3://$bucket_name/tenant-$tier-cf.yaml" --expires-in 600)
+
+aws cloudformation create-stack --stack-name "tenantTemplateStack-$tenantName" \
+  --template-url "$bucket_url" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=TenantName,ParameterValue=$tenantName \
+               ParameterKey=SuperUserEmail,ParameterValue=$email \
+               ParameterKey=SuperUserPassword,ParameterValue=$tenantName
+aws cloudformation wait stack-create-complete --stack-name "tenantTemplateStack-$tenantName"
+
+alb_dns=$(aws cloudformation describe-stacks \
+    --stack-name "tenantTemplateStack-$tenantName" \
+    --query "Stacks[0].Outputs[?contains(OutputKey,'ALBDnsName')].OutputValue" \
+    --output text)
+
+export tenantAppUrl=$alb_dns
+export tenantStatus="created"
+export registrationStatus="created"
